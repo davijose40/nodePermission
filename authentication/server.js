4d5991c2f2c6f3005a73d6811d3cf3fd188b8846
davijose <davijose40@gmail.com>
2020-06-07 15:51:16 -0300

    organize files, make basic authentication ready-to-go

--- authentication/server.js
+++ authentication/server.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const app = express()
 const { ROLE, users } = require('./data')
-const { authUser, authRole } = require('./basicAuth')
+const { authUser, authRole } = require('./basicAuth');
 const projectRouter = require('./routes/projects')
+
 
 app.use(express.json())
 app.use(setUser)
@@ -16,7 +17,7 @@ app.get('/dashboard', authUser, (req, res) => {
   res.send('Dashboard Page')
 })
 
-app.get('/admin', authUser, authRole(ROLE.ADMIN), (req, res) => {
+app.get('/admin',authUser, authRole(ROLE.ADMIN), (req, res) => {
   res.send('Admin Page')
 })
 
